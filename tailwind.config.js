a99128efbf924fd7dbeb29a816e41d00ea81d219
yigit ocalan <ocalanyigit3@gmail.com>
2024-04-29 15:16:37 +0300

    tailwind project finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container:
+    {
+      center:true,
+      screens:{
+        lg:'1140px',
+        xl:'1140px',
+        '2x1':'1140px',
+      }
+    }
+    ,
+    extend: {
+      fontFamily:{
+        gemunu:[ 'Gemunu Libre', 'sans-serif'],
+        open:['open sans','sans-serif']
+      },
+      colors:{
+        'gega-red':'#BC1A45',
+        'gega-blue':'#0000ff ',
+        'gega-gray':'#a9a9a9',
+        'gega-green':'#7fff00'
+      }
+    },
   },
   plugins: [],
 }
